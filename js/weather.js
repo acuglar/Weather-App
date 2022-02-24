7897857f5e5e717f5ebf7a8610a40d8949d7eb23
Alexandre Uglar <acuglar@gmail.com>
2022-02-23 21:41:44 -0300

    feat: get city data

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,21 @@
 const APIKey = 'g4CMSirPQGHFQ3UjW9fWUORQmcurD6ih'
+const getCityUrl = cityName => `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getCityData = async cityName => {
+  try {
+    const cityUrl = getCityUrl(cityName)
+    const response = await fetch(cityUrl)
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados')
+    }
+
+    const [cityData] = await response.json()
+    console.log(cityData)
+
+  } catch ({ name, message }) {
+    alert(`${name}: ${message}`)
+  }
+}
+
+getCityData('São Paulo')
