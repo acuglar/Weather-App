13619d4912a964d51fcfcb8b81bc996ec3412875
Alexandre Uglar <acuglar@gmail.com>
2022-02-24 17:53:03 -0300

    refactor app.js

--- js/app.js
+++ js/app.js
@@ -6,28 +6,30 @@ const cityCard = document.querySelector('[data-js="city-card"]')
 let timeImg = document.querySelector('[data-js="time"]')
 const timeIconContainer = document.querySelector('[data-js="time-icon"]')
 
-cityForm.addEventListener('submit', async event => {
-  event.preventDefault()
-
-  const inputValue = event.target.city.value
-  const [{ Key, LocalizedName }] = await getCityData(inputValue)
-  const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
-  const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
-
+const showcityCard = () => {
   if (cityCard.classList.contains('d-none')) {
     cityCard.classList.remove('d-none')
   }
+}
 
-  if (IsDayTime) {
-    timeImg.src = './src/day.svg'
-  } else {
-    timeImg.src = './src/night.svg'
-  }
+const showcityWeatherInfo = async cityName => {
+  const [{ Key, LocalizedName }] = await getCityData(cityName)
+  const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
+  const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
 
+  timeImg.src = IsDayTime ? './src/day.svg' : './src/night.svg'
   timeIconContainer.innerHTML = timeIcon
   cityNameContainer.textContent = LocalizedName
   cityWeatherContainer.textContent = WeatherText
   cityTemperatureContainer.textContent = Temperature.Metric.Value
+}
 
+cityForm.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const inputValue = event.target.city.value
+
+  showcityCard()
+  showcityWeatherInfo(inputValue)
   cityForm.reset()
 })
