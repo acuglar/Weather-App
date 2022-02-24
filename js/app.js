cbc8b3ad3bf118d60a4d44b99fc8b7e897ca4548
Alexandre Uglar <acuglar@gmail.com>
2022-02-24 15:19:23 -0300

    Add render data layout

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 const cityForm = document.querySelector('[data-js="change-location"]')
+const cityNameContainer = document.querySelector('[data-js="city-name"]')
+const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
+const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]')
 
 cityForm.addEventListener('submit', async event => {
   event.preventDefault()
 
   const inputValue = event.target.city.value
-  // debugger
-  console.log(inputValue);
   const [{ Key, LocalizedName }] = await getCityData(inputValue)
   const [{ WeatherText, Temperature }] = await getCityWeather(Key)
+  console.log(Temperature)
 
-  console.log(WeatherText, Temperature.Metric.Value)
+  cityNameContainer.textContent = LocalizedName
+  cityWeatherContainer.textContent = WeatherText
+  cityTemperatureContainer.textContent = Temperature.Metric.Value
+
   cityForm.reset()
 })
